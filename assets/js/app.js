092318e2b60ff56e95f8dcf4c3c3a9b81f9a8650
Joe Terlecki <joe.p.terlecki@gmail.com>
2021-11-02 16:31:18 -0400

    Finishin touches minus code explanation

--- assets/js/app.js
+++ assets/js/app.js
@@ -52,12 +52,19 @@ function displayFizzBuzzNumbers(fizzBuzzArray) {
     let tableRow = document.importNode(tableTemplate.content, true);
 
     let rowColumns = tableRow.querySelectorAll("td");
-
-
+    rowColumns[0].classList.add(fizzBuzzArray[number]);
     rowColumns[0].textContent = fizzBuzzArray[number];
+
+    rowColumns[1].classList.add(fizzBuzzArray[number + 1]);
     rowColumns[1].textContent = fizzBuzzArray[number + 1];
+
+    rowColumns[2].classList.add(fizzBuzzArray[number + 2]);
     rowColumns[2].textContent = fizzBuzzArray[number + 2];
+
+    rowColumns[3].classList.add(fizzBuzzArray[number + 3]);
     rowColumns[3].textContent = fizzBuzzArray[number + 3];
+
+    rowColumns[4].classList.add(fizzBuzzArray[number + 4]);
     rowColumns[4].textContent = fizzBuzzArray[number + 4];
 
     tableBody.appendChild(tableRow)
