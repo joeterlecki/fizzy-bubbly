256df643fb7ee5360664ca99482ab75500cca4fa
Joe Terlecki <joe.p.terlecki@gmail.com>
2022-04-11 14:35:28 -0400

    cleanup and refactor for python backend

---
+++ lambda_package/fizzy-bubbly-lambda/lambda_function.py
@@ -0,0 +1,52 @@
+def generate_number_list(start_number, end_number):
+    number_list = []
+    for number in range(start_number, end_number):
+        number_list.append(number)
+
+    return number_list
+
+
+def calculate_fiz_buzz(numbers):
+    fizz_buzz_list = []
+    for number in numbers:
+        if number % 3 == 0 and number % 5 == 0:
+            fizz_buzz_list.append('FIZZBUZZ')
+        elif number % 3 == 0:
+            fizz_buzz_list.append('FIZZ')
+        elif number % 5 == 0:
+            fizz_buzz_list.append('BUZZ')
+        else:
+            fizz_buzz_list.append(str(number))
+    return fizz_buzz_list
+
+
+def format_response(response_data):
+    numbers_response = {'response': response_data}
+
+    request_response = {'statusCode': 200,
+                        'headers': {
+                            'Content-Type': 'application/json',
+                            'Access-Control-Allow-Origin': '*'
+                        },
+                        'body': numbers_response
+                        }
+
+    return request_response
+
+
+def main(event):
+    start_number = int(event['queryStringParameters']['start_number'])
+    end_number = int(event['queryStringParameters']['end_number'])
+
+    number_list = generate_number_list(start_number, end_number + 1)
+    response = calculate_fiz_buzz(number_list)
+
+    request_response = format_response(response)
+
+    return request_response
+
+
+def lambda_handler(event, context):
+    response = main(event)
+
+    return response
